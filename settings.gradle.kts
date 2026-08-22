b9be407cd80a1900dbb5daf3b51798a1d424a2cc
Zephy <32806171+Zephy1@users.noreply.github.com>
2026-08-22 04:45:34 -0400

    Use local preprocessor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "essential-gradle-toolkit"
+
+includeBuild("../preprocessor") {
+    dependencySubstitution {
+        substitute(module("com.github.replaymod:preprocessor")).using(project(":"))
+    }
+}
